dfc3462182647ce6f87e3f70f57d75745163e73e
Vipin Gautam <95081432+vipingautam07@users.noreply.github.com>
2023-09-01 00:22:03 +0530

    Update Subscribe.jsx

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -2,7 +2,7 @@ import Button from "../components/Button";
 
 const Subscribe = () => {
   return (
-    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+    <section className="max-container flex justify-center items-center max-lg:flex-col gap-10" id="contact-us">
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up from
         <span className="text-coral-red"> Updates</span> & Newsletter
       </h3>
