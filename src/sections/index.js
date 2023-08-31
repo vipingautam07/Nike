87763c2d1ff5ac68c0c8b88e26c723fec76ffff8
Vipin Gautam <gautamvipin2002@gmail.com>
2023-09-01 00:13:28 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+
+export{
+    Hero,
+    CustomerReviews,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
